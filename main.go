bc3ef11d30e4a85814567533d6ea405f5fa07099
Mayank Sharma <mayank@circles.asia>
2019-09-10 12:21:18 +0800

    removed extra structs

--- main.go
+++ main.go
@@ -5,30 +5,6 @@ import (
 	config "go-config-read/config"
 )
 
-type Configuration struct {
-	Users  []string
-	Groups []string
-}
-
-type TimeZoneConfig struct {
-	timezoneList []*TimezoneList
-}
-
-type TimezoneList struct {
-	country map[string]*Country
-	// country Country
-}
-
-type Country struct {
-	state_pincode map[string]StatePincode
-	// state_pincode *StatePincode
-}
-
-type StatePincode struct {
-	timezone int
-	dst      int
-}
-
 func main() {
 
 	fmt.Println("in main first")
